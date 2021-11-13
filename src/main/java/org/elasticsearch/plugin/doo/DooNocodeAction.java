a93f51e0d85eb1c3bcae764a9ca49b1a0f2ba4a6
900gle <900gle.hupa@gmail.com>
2021-11-13 18:08:54 +0900

    init

---
+++ src/main/java/org/elasticsearch/plugin/doo/DooNocodeAction.java
@@ -0,0 +1,63 @@
+package org.elasticsearch.plugin.doo;
+
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.BytesRestResponse;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestRequest;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static org.elasticsearch.rest.RestRequest.Method.GET;
+import static org.elasticsearch.rest.RestStatus.OK;
+
+public class DooNocodeAction extends BaseRestHandler {
+
+    public DooNocodeAction(final Settings settings,
+                               final RestController controller) {
+        // nothing
+    }
+
+
+    @Override
+    public String getName() {
+        return "nocode_doo_action.";
+    }
+
+    @Override
+    public List<Route> routes() {
+        return Collections.unmodifiableList(Arrays.asList(
+                new Route(GET, "/{index}/_nocode"),
+                new Route(GET, "/_nocode")));
+    }
+
+
+    @Override
+    protected RestChannelConsumer prepareRequest(final RestRequest request,
+                                                 final NodeClient client) throws IOException {
+        final boolean isPretty = request.hasParam("pretty");
+        final String index = request.param("index");
+        return channel -> {
+            final XContentBuilder builder = JsonXContent.contentBuilder();
+            if (isPretty) {
+                builder.prettyPrint().lfAtEnd();
+            }
+            builder.startObject();
+            if (index != null) {
+                builder.field("index", index);
+            }
+            builder.field("description",
+                    "This is a Doo response: " + new Date().toString());
+            builder.endObject();
+            channel.sendResponse(new BytesRestResponse(OK, builder));
+        };
+    }
+
+}
---
+++ src/main/java/org/elasticsearch/plugin/doo/DooPlugin.java
@@ -0,0 +1,30 @@
+package org.elasticsearch.plugin.doo;
+
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class DooPlugin extends Plugin implements ActionPlugin {
+    @Override
+    public List<RestHandler> getRestHandlers(final Settings settings,
+                                             final RestController restController,
+                                             final ClusterSettings clusterSettings,
+                                             final IndexScopedSettings indexScopedSettings,
+                                             final SettingsFilter settingsFilter,
+                                             final IndexNameExpressionResolver indexNameExpressionResolver,
+                                             final Supplier<DiscoveryNodes> nodesInCluster) {
+        return Arrays.asList(
+                new DooNocodeAction(settings, restController));
+    }
+}
